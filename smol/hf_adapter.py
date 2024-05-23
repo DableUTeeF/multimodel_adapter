96405aa29736f08e682660d66652b4b3b25a492d
DableUTeeF <palm22180@gmail.com>
2024-05-23 15:15:39 +0700

    Sort of finished but untested

--- smol/hf_adapter.py
+++ smol/hf_adapter.py
@@ -1,7 +1,194 @@
 import torch.nn as nn
 import clip
-from transformers import AutoModelForCausalLM
+from transformers.models.llama.modeling_llama import (
+    LlamaMLP,
+    LlamaForCausalLM,
+    LlamaDecoderLayer,
+    LlamaAttention,
+    LlamaModel,
+    ACT2FN,
+    Cache,
+    DynamicCache,
+    repeat_kv,
+    _prepare_4d_causal_attention_mask,
+    apply_rotary_pos_emb
+)
+from torch.nn import functional as F
+import math
 from timm.models.vision_transformer import Block
+import torch
+
+
+def llmlp_init(self, config):
+    super().__init__()
+    self.config = config
+    self.hidden_size = config.hidden_size
+    self.intermediate_size = config.intermediate_size
+    self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=config.attention_bias)
+    self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=config.attention_bias)
+    self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=config.attention_bias)
+    self.act_fn = ACT2FN[config.hidden_act]
+
+
+def llama_layer_fwd(
+        self,
+        hidden_states,
+        attention_mask=None,
+        position_ids=None,
+        past_key_value=None,
+        output_attentions=False,
+        use_cache=False,
+        adapter=None,
+        **kwargs,
+):
+    residual = hidden_states
+
+    hidden_states = self.input_layernorm(hidden_states)
+
+    # Self Attention
+    hidden_states, self_attn_weights, present_key_value = self.self_attn(
+        hidden_states=hidden_states,
+        attention_mask=attention_mask,
+        position_ids=position_ids,
+        past_key_value=past_key_value,
+        output_attentions=output_attentions,
+        use_cache=use_cache,
+        adapter=adapter,
+        **kwargs,
+    )
+    hidden_states = residual + hidden_states
+
+    # Fully Connected
+    residual = hidden_states
+    hidden_states = self.post_attention_layernorm(hidden_states)
+    hidden_states = self.mlp(hidden_states)
+    hidden_states = residual + hidden_states
+    outputs = (hidden_states,)
+    if output_attentions:
+        outputs += (self_attn_weights,)
+    if use_cache:
+        outputs += (present_key_value,)
+    return outputs
+
+
+def llama_attention_fwd(
+        self,
+        hidden_states,
+        attention_mask=None,
+        position_ids=None,
+        past_key_value=None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        adapter=None,
+        **kwargs,
+):
+    bsz, q_len, _ = hidden_states.size()
+
+    if self.config.pretraining_tp > 1:
+        key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
+        query_slices = self.q_proj.weight.split(
+            (self.num_heads * self.head_dim) // self.config.pretraining_tp, dim=0
+        )
+        key_slices = self.k_proj.weight.split(key_value_slicing, dim=0)
+        value_slices = self.v_proj.weight.split(key_value_slicing, dim=0)
+
+        query_states = [F.linear(hidden_states, query_slices[i]) for i in range(self.config.pretraining_tp)]
+        query_states = torch.cat(query_states, dim=-1)
+
+        key_states = [F.linear(hidden_states, key_slices[i]) for i in range(self.config.pretraining_tp)]
+        key_states = torch.cat(key_states, dim=-1)
+
+        value_states = [F.linear(hidden_states, value_slices[i]) for i in range(self.config.pretraining_tp)]
+        value_states = torch.cat(value_states, dim=-1)
+
+    else:
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+
+    query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+    key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+    value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+    kv_seq_len = key_states.shape[-2]
+    if past_key_value is not None:
+        if self.layer_idx is None:
+            raise ValueError(
+                f"The cache structure has changed since version v4.36. If you are using {self.__class__.__name__} "
+                "for auto-regressive decoding with k/v caching, please make sure to initialize the attention class "
+                "with a layer index."
+            )
+        kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
+    cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
+    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+
+    if adapter is not None:
+        adapter_len = adapter.shape[1]
+        adapter_v = self.v_proj(adapter).view(bsz, adapter_len, self.n_local_heads, self.head_dim)
+        adapter_v = adapter_v.transpose(1, 2)
+
+        if adapter_len > 1:
+            adapter_k = self.k_proj(adapter).view(bsz, adapter_len, self.n_local_heads, self.head_dim)
+            adapter_k = adapter_k.transpose(1, 2)
+
+    if past_key_value is not None:
+        cache_kwargs = {"sin": sin, "cos": cos}  # Specific to RoPE models
+        key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+    key_states = repeat_kv(key_states, self.num_key_value_groups)
+    value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+    attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+
+    if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
+        raise ValueError(
+            f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
+            f" {attn_weights.size()}"
+        )
+
+    if attention_mask is not None:
+        if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+            raise ValueError(
+                f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+            )
+        attn_weights = attn_weights + attention_mask
+
+    # upcast attention to fp32
+    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+    attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+    attn_output = torch.matmul(attn_weights, value_states)
+
+    if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+        raise ValueError(
+            f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+            f" {attn_output.size()}"
+        )
+
+    if adapter is not None:
+        if adapter_len > 1:
+            adapter_scores = torch.matmul(query_states, adapter_k.transpose(2, 3)) / math.sqrt(self.head_dim)
+            adapter_scores = self.gate.tanh() * F.softmax(adapter_scores.float(), dim=-1).type_as(query_states)
+            if self.w_new_gate:
+                adapter_scores = self.new_gate * adapter_scores
+            attn_output = attn_output + torch.matmul(adapter_scores, adapter_v)
+        else:
+            attn_output = attn_output + self.gate.tanh() * adapter_v
+
+    attn_output = attn_output.transpose(1, 2).contiguous()
+
+    attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
+
+    if self.config.pretraining_tp > 1:
+        attn_output = attn_output.split(self.hidden_size // self.config.pretraining_tp, dim=2)
+        o_proj_slices = self.o_proj.weight.split(self.hidden_size // self.config.pretraining_tp, dim=1)
+        attn_output = sum([F.linear(attn_output[i], o_proj_slices[i]) for i in range(self.config.pretraining_tp)])
+    else:
+        attn_output = self.o_proj(attn_output)
+
+    if not output_attentions:
+        attn_weights = None
+
+    return attn_output, attn_weights, past_key_value
 
 
 class HF_adapter(nn.Module):
@@ -19,7 +206,7 @@ class HF_adapter(nn.Module):
         w_bias = phase == "finetune"
 
         self.clip, self.clip_transform = clip.load(clip_model)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+        self.model = LlamaForCausalLM.from_pretrained(model_name, attention_bias=w_bias)
 
         clip_dim = self.clip.visual.proj.shape[1]
         self.clip_proj = nn.Linear(clip_dim, v_embed_dim)
@@ -68,3 +255,120 @@ class HF_adapter(nn.Module):
         else:
             raise ValueError(f"Unknown model phase: {phase}")
 
+    def clip_encode_image(self, x):
+        # modified from CLIP
+        x = self.clip.visual.conv1(x)  # shape = [*, width, grid, grid]
+        # shape = [*, width, grid ** 2]
+        x = x.reshape(x.shape[0], x.shape[1], -1)
+        x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
+        x = torch.cat([self.clip.visual.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1,
+                                                                                  x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)  # shape = [*, grid ** 2 + 1, width]
+        x = x + self.clip.visual.positional_embedding.to(x.dtype)
+        x = self.clip.visual.ln_pre(x)
+
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.clip.visual.transformer(x)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+
+        # preserve all spatial tokens
+        x = self.clip.visual.ln_post(x[:, :, :])
+
+        if self.clip.visual.proj is not None:
+            x = x @ self.clip.visual.proj
+
+        return x
+
+    def forward_visual(self, imgs):
+        clip_feats = self.clip_encode_image(imgs)
+        clip_feats = self.clip_proj_norm(self.clip_proj(clip_feats.float()))
+
+        visual_query = self.visual_query.weight.unsqueeze(
+            0).repeat(len(imgs), 1, 1)
+        visual_query = torch.cat([visual_query, clip_feats], dim=1)
+        for block in self.visual_blocks:
+            visual_query = block(visual_query)
+
+        visual_query = visual_query[:, :self.query_len, :]
+        visual_query = self.visual_proj(visual_query)
+        visual_query = self.visual_proj_norm(visual_query)
+
+        return visual_query
+
+    def forward(self, input_ids, labels, imgs):
+        visual_query = self.forward_visual(imgs)
+
+        batch_size, seq_length = input_ids.shape[:2]
+
+        attention_mask = _prepare_4d_causal_attention_mask(
+            None, (batch_size, seq_length), None, None
+        )
+        position_ids = torch.arange(
+            0, seq_length + 0, dtype=torch.long, device=input_ids.device
+        )
+        position_ids = position_ids.unsqueeze(0)
+        inputs_embeds = self.model.model.embed_tokens(input_ids)
+        hidden_states = inputs_embeds
+        for decoder_layer in self.model.model.layers[:-1 * self.query_layer]:
+            if self.gradient_checkpointing and self.training:
+                layer_outputs = self._gradient_checkpointing_func(
+                    decoder_layer.__call__,
+                    hidden_states,
+                    attention_mask,
+                    position_ids,
+                    None,
+                    False,
+                    False,
+                )
+            else:
+                layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=attention_mask,
+                    position_ids=position_ids,
+                    past_key_value=None,
+                    output_attentions=False,
+                    use_cache=False,
+                )
+            hidden_states = layer_outputs[0]
+
+        adapter = self.adapter_query.weight.reshape(self.query_layer, self.query_len, -1).unsqueeze(1)
+        adapter_index = 0
+
+        for decoder_layer in self.model.model.layers[-1 * self.query_layer:]:
+            dynamic_adapter = adapter[adapter_index].repeat(batch_size, 1, 1)
+            dynamic_adapter = dynamic_adapter + visual_query
+            if self.gradient_checkpointing and self.training:
+                layer_outputs = self._gradient_checkpointing_func(
+                    decoder_layer.__call__,
+                    hidden_states,
+                    attention_mask,
+                    position_ids,
+                    None,
+                    False,
+                    True,
+                    dynamic_adapter
+                )
+            else:
+                layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=attention_mask,
+                    position_ids=position_ids,
+                    past_key_value=None,
+                    output_attentions=False,
+                    use_cache=True,
+                    adapter=dynamic_adapter
+                )
+            hidden_states = layer_outputs[0]
+        hidden_states = self.model.norm(hidden_states)
+        output = self.model.lm_head(hidden_states)
+        output = output[:, :-1, :]
+        labels = labels[:, 1:]
+        if labels.sum() == 0:
+            c_loss = output.mean() * 0
+        else:
+            c_loss = self.criterion(output.reshape(-1, self.model.vocab_size), labels.flatten())
+        return c_loss, c_loss
+
+
+LlamaMLP.__init__ = llmlp_init
+LlamaDecoderLayer.forward = llama_layer_fwd
+LlamaAttention.forward = llama_attention_fwd
